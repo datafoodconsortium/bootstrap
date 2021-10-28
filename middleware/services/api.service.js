70713b10b3148a734fe12c00d01118dd401d91f0
Maxime Lecoq <maxime@lecoqlibre.fr>
2021-10-29 00:16:42 +0200

    Retrocompability with 1.5 & 1.6

--- middleware/services/api.service.js
+++ middleware/services/api.service.js
@@ -1,21 +1,36 @@
 const ApiGatewayService = require('moleculer-web');
 const E = require("moleculer-web").Errors;
+const path = require('path');
 
 module.exports = {
   mixins: [ApiGatewayService],
   settings: {
     server: true,
     routes: [
+      // {
+      //   path: '/ontology',
+      //   use: [
+      //     ApiGatewayService.serveStatic('./public/ontology.ttl', {
+      //       setHeaders: res => {
+      //         res.setHeader('Access-Control-Allow-Origin', '*');
+      //         res.setHeader('Content-Type', 'text/turtle; charset=utf-8');
+      //       }
+      //     })
+      //   ]
+      // },
       {
-        path: '/ontology',
-        use: [
-          ApiGatewayService.serveStatic('./public/ontology.ttl', {
-            setHeaders: res => {
-              res.setHeader('Access-Control-Allow-Origin', '*');
-              res.setHeader('Content-Type', 'text/turtle; charset=utf-8');
-            }
-          })
-        ]
+        path: "/1.5/",
+        authentication: true,
+        aliases: {
+          "/": "api.getVersion"
+        }
+      },
+      {
+        path: "/1.6/",
+        authentication: true,
+        aliases: {
+          "/": "api.getVersion"
+        }
       }
     ],
     cors: {
@@ -30,6 +45,17 @@ module.exports = {
       const payload = await ctx.call('auth.authenticate', { route, req, res } );
       if( payload == null )
         return Promise.reject(new E.UnAuthorizedError(E.ERR_INVALID_TOKEN));
+    }
+  },
+  actions: {
+    getVersion(ctx) {
+      let url = ctx.options.parentCtx.params.req.baseUrl;
+      let version = url.replace(/\//g, '');
+      ctx.meta.$responseHeaders = {
+        "Content-Type": `application/ld+json; charset=utf-8`
+      };
+      let json = require(path.resolve(__dirname, '../public/'+version+'.json'));
+      return json;
     }
   }
 };
